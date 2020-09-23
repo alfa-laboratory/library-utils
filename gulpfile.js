0d7d8e314ea6580f9fce9752c0fe4bfefb1f52cb
Dmitriy Nikitin <dmitriy.nikitin90@gmail.com>
2020-09-23 22:14:36 +0300

    fix(gulpfile): removed empty line

--- gulpfile.js
+++ gulpfile.js
@@ -5,5 +5,4 @@ const createTasks = require('./gulp-tasks');
 const packageJsonPath = path.resolve(process.cwd(), 'package.json');
 const appPackage = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
 
-
 createTasks(appPackage.name, appPackage['library-utils']);
